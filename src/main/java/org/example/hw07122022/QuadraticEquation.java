bda8cceba4c1d45d14e80b3548c1478c3f908f0f
andreev-sp@serviceit.su <andreev-sp@serviceit.su>
2022-12-17 11:15:05 +0400

    implementation

---
+++ src/main/java/org/example/hw07122022/QuadraticEquation.java
@@ -0,0 +1,35 @@
+package org.example.hw07122022;
+
+public class QuadraticEquation {
+    public static final double eps = 0.0001;
+
+    public static boolean isEquals(double valueFirst, double valueSecond) {
+        return Math.abs(valueFirst - valueSecond) < eps;
+    }
+
+    public double[] solve(double a, double b, double c) {
+        if (isEquals(a, 0)) {
+            throw new RuntimeException("Корней нет");
+        }
+        validateDouble(a, b, c);
+        double discriminant = b * b - 4 * a * c;
+        if (isEquals(discriminant, 0)) {
+            return new double[]{-b / (2 * a), -b / (2 * a)};
+        } else if (discriminant > 0) {
+            return new double[]{(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)};
+        } else {
+            return new double[0];
+        }
+    }
+
+    private void validateDouble(double... values) {
+        for (double value : values) {
+            if (Double.isInfinite(value)) {
+                throw new RuntimeException("Указано бесконечное число");
+            }
+            if (Double.isNaN(value)) {
+                throw new RuntimeException("Указано неопределенное число");
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/hw07122022/QuadraticEquationTest.java
@@ -0,0 +1,113 @@
+package org.example.hw07122022;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class QuadraticEquationTest {
+
+    /**
+     * 3 Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+     */
+    @Test
+    void solveEmpty() {
+        QuadraticEquation main = new QuadraticEquation();
+        double[] solve = main.solve(1, 0, 1);
+        Assertions.assertEquals(solve.length, 0);
+    }
+
+    /**
+     * 5 Написать тест, который проверяет, что для уравнения x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)
+     */
+    @Test
+    void solveTwoResult() {
+        QuadraticEquation main = new QuadraticEquation();
+        double[] solve = main.solve(1, 0, -1);
+        Assertions.assertEquals(solve.length, 2);
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[0], 1));
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[1], -1));
+    }
+
+    /**
+     * 7 Написать тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1).
+     */
+    @Test
+    void solveTwoEqualsResult() {
+        QuadraticEquation main = new QuadraticEquation();
+        double[] solve = main.solve(1, 2, 1);
+        Assertions.assertEquals(solve.length, 2);
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[0], -1));
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[1], -1));
+    }
+
+    /**
+     * 9 Написать тест, который проверяет, что коэффициент a не может быть равен 0. В этом случае solve выбрасывает исключение.
+     */
+    @Test
+    void solveZeroA() {
+        QuadraticEquation main = new QuadraticEquation();
+        try {
+            main.solve(0, 2, 1);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+    }
+
+    /**
+     * 7 Написать тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1).
+     */
+    @Test
+    void solveSmallDiscriminant() {
+        QuadraticEquation main = new QuadraticEquation();
+        double[] solve = main.solve(1, 2, 0.999999999);
+        Assertions.assertEquals(solve.length, 2);
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[0], -1));
+        Assertions.assertTrue(QuadraticEquation.isEquals(solve[1], -1));
+    }
+
+    /**
+     * 13 Посмотреть какие еще значения могут принимать числа типа double,
+     * кроме числовых и написать тест с их использованием на все коэффициенты. solve должен выбрасывать исключение.
+     */
+    @Test
+    void solveCustom() {
+        QuadraticEquation main = new QuadraticEquation();
+        try {
+            main.solve(Double.POSITIVE_INFINITY, 2, 1);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+        try {
+            main.solve(0, Double.POSITIVE_INFINITY, 1);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+        try {
+            main.solve(0, 2, Double.POSITIVE_INFINITY);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+        try {
+            main.solve(Double.NaN, 2, 1);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+        try {
+            main.solve(0, Double.NaN, 1);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+        try {
+            main.solve(0, 2, Double.NaN);
+            Assertions.fail();
+        } catch (Exception ignore) {
+        }
+    }
+
+    @Test
+    void isEquals() {
+        Assertions.assertTrue(QuadraticEquation.isEquals(0,0));
+        Assertions.assertTrue(QuadraticEquation.isEquals(0,0 + QuadraticEquation.eps / 2));
+        Assertions.assertFalse(QuadraticEquation.isEquals(1,2));
+
+    }
+}
